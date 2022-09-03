d8e1d47f444070d26e802cabbbeda2e37512a13a
Anderson1 <anderson.santos@vlcode.com.br>
2022-09-03 16:53:46 -0300

    teste-vue-todo

---
+++ src/views/SobreView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+<script>
+  import Vue from 'vue'
+  
+export default Vue.extend({
+  name: 'SobreView',
+})
+</script>
---
+++ src/views/TarefasView.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <v-col
+          cols="12"
+        >
+          <v-text-field
+            v-model="campoInput"
+            label="Qual sua tarefa"
+            outlined
+            clearable
+            @keyup.enter="handleAddTarefa()"
+          ></v-text-field>
+        </v-col>
+    <v-list
+      flat
+      subheader
+   >
+
+      <v-list-item-group
+        multiple
+        active-class=""
+      >
+      <div
+        v-for="tarefa, index in $store.state.tarefas"
+        :key="index"
+      >
+        <TarefaView 
+          :tarefa="tarefa"
+        />
+
+      </div>
+        
+      </v-list-item-group>
+    </v-list>
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue'
+  import TarefaView from '../components/tarefas/TarefaView.vue'
+  
+
+  export default Vue.extend({
+    name: 'TarefasView',
+    components:{
+      TarefaView
+    }, 
+    data() {
+      return {
+        campoInput: null,
+      }
+    },
+  methods: {
+    handleAddTarefa(){
+      this.$store.commit('adicionaTarefa', this.campoInput),
+      this.campoInput = null;
+            }
+        }
+    })
+  
+</script>
